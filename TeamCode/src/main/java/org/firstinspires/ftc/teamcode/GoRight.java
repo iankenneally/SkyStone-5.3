09e7b2d157b3833b2aab60aa2573aa858a124742
iankenneally <iankenneally160@gmail.com>
2019-11-17 00:49:19 -0800

    big meet 1 fixes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoRight.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="right", group="Knightbot")
+//@Disabled
+public class GoRight extends LinearOpMode {
+
+    HardwareKnightbot         robot   = new HardwareKnightbot();   // Use a Knightbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+
+        waitForStart();
+        parallelRight(1000);
+        stopMotors();
+    }
+
+    private void forward(int time) {
+            robot.frontLeft.setPower(-robot.FL_POWER);
+            robot.frontRight.setPower(-robot.FR_POWER);
+            robot.backLeft.setPower(-robot.BL_POWER);
+            robot.backRight.setPower(-robot.BR_POWER);
+            sleep(time);
+            stopMotors();
+        }
+
+    private void backward(int time) {
+            robot.frontLeft.setPower(robot.FL_POWER);
+            robot.frontRight.setPower(robot.FR_POWER);
+            robot.backLeft.setPower(robot.BL_POWER);
+            robot.backRight.setPower(robot.BR_POWER);
+            sleep(time);
+            stopMotors();
+        } 
+
+        private void turnLeft(int time) {
+            robot.frontLeft.setPower(robot.FL_POWER);
+            robot.frontRight.setPower(-robot.FR_POWER);
+            robot.backLeft.setPower(robot.BL_POWER);
+            robot.backRight.setPower(-robot.BR_POWER);
+            sleep(time);
+            stopMotors();
+        }
+
+        private void turnRight(int time) {
+            robot.frontLeft.setPower(-robot.FL_POWER);
+            robot.frontRight.setPower(robot.FR_POWER);
+            robot.backLeft.setPower(-robot.BL_POWER);
+            robot.backRight.setPower(robot.BR_POWER);
+            sleep(time);
+            stopMotors();
+        }
+
+        private void parallelLeft(int time) {
+            robot.frontLeft.setPower(robot.FL_POWER);
+            robot.frontRight.setPower(-robot.FR_POWER);
+            robot.backLeft.setPower(-robot.BL_POWER);
+            robot.backRight.setPower(robot.BR_POWER);
+            sleep(time);
+            stopMotors();
+        }
+
+        private void parallelRight(int time) {
+            robot.frontLeft.setPower(-robot.FL_POWER);
+            robot.frontRight.setPower(robot.FR_POWER);
+            robot.backLeft.setPower(robot.BL_POWER);
+            robot.backRight.setPower(-robot.BR_POWER);
+            sleep(time);
+            stopMotors();
+        }
+
+        private void stopMotors() {
+            robot.frontLeft.setPower(0);
+            robot.frontRight.setPower(0);
+            robot.backLeft.setPower(0);
+            robot.backRight.setPower(0);
+        }
+
+        private void openClaw() {
+            robot.leftClaw.setPosition(1);
+            robot.rightClaw.setPosition(0);
+        }
+
+        private void closeClaw() {
+            robot.leftClaw.setPosition(.4);
+            robot.rightClaw.setPosition(1);
+        }
+}
